082f676c414932dbf658097f46dd1397d00a9829
mattMusser <matthew.musser@gmail.com>
2017-03-30 16:25:28 -0500

    Checkpoint 10 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,23 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function (points) {
+    var points = document.getElementsByClassName('point');
 
     var revealPoint = function (index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
-    for (var i = 0; i < points.length; i++){
-        revealPoint(i);
-    }
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
-    // Automatically animate the points on a tall screen whree scrolling can't trigger the animation
+     //Automatically animate the points on a tall screen whree scrolling can't trigger the animation
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+    var sellingPoints = document.getElementsByC lassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {

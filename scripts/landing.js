587e7f96281fea9e8e83fe2b205258957c97e9e5
mattMusser <matthew.musser@gmail.com>
2017-03-30 14:24:22 -0500

    Assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,12 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function (points) {
 
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function (element) {
+        element.style.opacity = 1;
+        element.style.transform = "scaleX(1) translateY(0)";
     }
 
-    for (var i = 0; i < points.length; i++){
-        revealPoint(i);
-    }
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
@@ -19,8 +15,8 @@ window.onload = function() {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+    var sellingPoints = document.getElementsByC lassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {

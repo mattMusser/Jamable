43ee6415a686bf3e6715150b4ab8bf95dbef1c87
mattMusser <matthew.musser@gmail.com>
2017-03-29 22:30:12 -0500

    Checkpoint 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,30 @@
-var animatePoints = function () {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-    var revealFirstPoint = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+var animatePoints = function (points) {
 
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function (index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealPoint = function(pointsIndex) {
-        var pointsIndex = points[i];
-        for(var i = 0; i<3; i++) {
-            revealFirstPoint();
-            revealSecondPoint();
-            revealThirdPoint();
-        }
-    };
+    for (var i = 0; i < points.length; i++){
+        revealPoint(i);
+    }
 };
+
+window.onload = function() {
+    // Automatically animate the points on a tall screen whree scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+        }
+    });
+}

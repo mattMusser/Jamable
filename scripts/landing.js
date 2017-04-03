cfa07b62e681786fa1759d134d98a237b4fc8de9
mattMusser <matthew.musser@gmail.com>
2017-04-02 21:07:52 -0500

    Checkpoint 11 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -17,7 +17,7 @@ window.onload = function() {
         animatePoints(pointsArray);
     }
 
-    var sellingPoints = document.getElementsByC lassName('selling-points')[0];
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {

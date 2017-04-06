6920c5e8faa1144959e403306483fb412f4cb2f6
mattMusser <matthew.musser@gmail.com>
2017-04-06 17:11:18 -0500

    fixed albumArtUrl in album.js

--- scripts/album.js
+++ scripts/album.js
@@ -4,7 +4,7 @@ var albumPicasso = {
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtUrl: 'assets\images\album_covers\01.png',
+    albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
       { title: 'Blue', duration: '4:26' },
       { title: 'Green', duration: '3:14' },
@@ -102,7 +102,7 @@ window.onload = function() {
 
 for (var i = 0; i < songRows.length; i++) {
     songRows[i].addEventListener('mouseleave', function(event) {
-        
+
         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
     });
 }

117ff3e4bcb45365d08a09c58f71b27184b6806c
mattMusser <matthew.musser@gmail.com>
2017-05-13 16:47:29 -0500

    Assignment 21

--- scripts/album.js
+++ scripts/album.js
@@ -193,6 +193,26 @@ var setupSeekBars = function() {
     });
 };
 
+var setCurrenttimeInPlayBar = function(currentTime) { //#1
+    //Sets the text of the element with the .current-time class to the current time in the song
+
+    //Add the method to updateSeekBarWhileSongPlays() so the current time updates with song playback
+ };
+
+ var setTotalTimeInPlayerBar = function(totalTime) { //#2
+     //Sets the text of the element with the .total-time class to the length of the song.
+
+     //Add the method to updatePlayerBarSong() so the total time is set when a song first plays.
+ };
+
+ var filterTimeCode = function(timeInSeconds) { //#3
+     //Uses the parseFloat() method to get the seconds in number form
+
+     //Store variables for whole seconds and whole minutes
+
+     //Return time in the formate X:XX
+ };
+
 var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
 };
@@ -257,3 +277,33 @@ var previousSong = function () {
     $lastSongNumberCell.html(lastSongNumber);
 
 };
+
+var updatePlayerBarSong = function() {
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+};
+
+
+//Load the first album, Picasso, by default.
+var currentAlbumIndex = 0;
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauseButton = '<span class="ion-pause"></span>';
+
+//Store current song and album information.
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
+
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
+
+$(document).ready(function() {
+    //setCurrentAlbum(albums[currentAlbumIndex]);
+    setCurrentAlbum(albumPicasso);
+    $previousButton.click(previousSong);
+    $nextButton.click(nextSong);
+});
